d14beee590f88f1ada023562aa49a7d03ac9c501
Paul Setzer <1337JiveTurkey@gmail.com>
2021-12-06 23:30:02 -0500

    Day 2 2019 Intcode first implementation

--- src/tyool2019/Day2.scala
+++ src/tyool2019/Day2.scala
@@ -1,5 +1,34 @@
 package tyool2019
 
 object Day2 extends Main {
+	def main(args: Array[String]): Unit = {
+		star4()
+	}
 
+	def star3(): Unit = {
+		val intcode = new Intcode(fileLine("Day2.txt"))
+		intcode.mem(1) = 12
+		intcode.mem(2) = 2
+		for (line <- intcode) {
+			println(line)
+		}
+		println(intcode.mem(0))
+	}
+
+	def star4(): Unit = {
+		for (i <- 0 to 99) {
+			for (j <- 0 to 99) {
+				val intcode = new Intcode(fileLine("Day2.txt"))
+				intcode.mem(1) = i
+				intcode.mem(2) = j
+				for (line <- intcode) {
+					// Run silently
+				}
+				val result = intcode.mem(0)
+				if (result == 19690720) {
+					println(s"$result found at $i$j")
+				}
+			}
+		}
+	}
 }
--- src/tyool2019/Intcode.scala
+++ src/tyool2019/Intcode.scala
@@ -2,33 +2,88 @@ package tyool2019
 
 import scala.collection.mutable
 
-class Intcode(init: String) extends Iterator[Option[String]] {
+class Intcode(init: String) extends Iterator[String] {
 	val mem: mutable.Buffer[Int] = init.split(",").flatMap(_.toIntOption).toBuffer
 
 	// Program counter
 	var pc = 0
-	var terminated = false
-	var error = ""
-
-	override def hasNext: Boolean = !terminated && error.isBlank
-
-	override def next(): Option[String] = {
-		Some("Step complete")
+	var nextOp: OpCode = null;
+	{
+		val (newOp, pcIncrement) = decode(pc)
+		pc += pcIncrement
+		nextOp = newOp
 	}
 
-	private def decode(position: Int): OpCode = {
-		val op = mem(position)
+	override def hasNext: Boolean = !nextOp.isTerminal
+
+	override def next(): String = {
+		val executing = nextOp
+		val (newOp, pcIncrement) = decode(pc)
+		pc += pcIncrement
+		nextOp = newOp
+		executing.op(mem)
+	}
+
+	private def decode(at: Int): (OpCode, Int) = {
+		val op = mem(at)
 		val instruction = op % 100
 		val flags = (op / 100) % 100
 
-		Terminate
+		instruction match {
+			case 1 => (Add(at, mem(at + 1), mem(at + 2), mem(at + 3)), 4)
+			case 2 => (Mul(at, mem(at + 1), mem(at + 2), mem(at + 3)), 4)
+			case 99 => (Terminate(at), 1)
+			case invalid => (Invalid(invalid, at), 1)
+		}
 	}
-}
 
-trait OpCode {
-	val code: Int
-}
+	// Common parent of opcodes
+	trait OpCode {
+		val at: Int
+		val code: Int
+		val name: String
+		def header: String = s" $name (@$at): "
+		val isTerminal = false;
 
-case object Terminate extends OpCode {
-	override val code: Int = 99
+		def op(mem: mutable.Buffer[Int]): String
+	}
+
+	case class Add(at: Int, read1: Int, read2: Int, write: Int) extends OpCode {
+		val code = 1
+		val name = "ADD"
+
+		override def op(mem: mutable.Buffer[Int]): String = {
+			val value1 = mem(read1)
+			val value2 = mem(read2)
+			mem(write) = value1 + value2
+			header + s"@$write = $value1 (@$read1) + $value2 (@$read2)"
+		}
+	}
+
+	case class Mul(at: Int, read1: Int, read2: Int, write: Int) extends OpCode {
+		val code = 2
+		val name = "MUL"
+
+		override def op(mem: mutable.Buffer[Int]): String = {
+			val value1 = mem(read1)
+			val value2 = mem(read2)
+			mem(write) = value1 * value2
+			header + s"@$write = $value1 (@$read1) * $value2 (@$read2)"
+		}
+	}
+
+	case class Terminate(at: Int) extends OpCode {
+		val code = 99
+		val name = "TRM"
+		override val isTerminal = true;
+
+		override def op(mem: mutable.Buffer[Int]): String = header + "Terminated"
+	}
+	case class Invalid(code: Int, at: Int) extends OpCode {
+		val name = "XXX"
+		override val isTerminal = true;
+
+		override def op(mem: mutable.Buffer[Int]): String = header + s"Invalid Instruction $code"
+	}
+
 }
