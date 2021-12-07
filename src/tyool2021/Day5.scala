e2cf815a0ce01ad992bf46084d04d1f2ebef03f5
Paul Setzer <1337JiveTurkey@gmail.com>
2021-12-06 21:38:41 -0500

    Day 5 2021 Fix (Inclusive dammit)

--- src/tyool2021/Day5.scala
+++ src/tyool2021/Day5.scala
@@ -17,13 +17,18 @@ object Day5 extends Main {
 	val dRegex: Regex = """(\d+),(\d+) -> (\d+),(\d+)""".r
 
 	def star9(): Unit = {
-		val lines = fileLines("Star9prime.txt")
+		val lines = fileLines("Star9.txt")
 		val parsedLines = (lines flatMap {
 			case vRegex(x, y1, y2) => Some(VLine(x.toInt, Math.min(y1.toInt, y2.toInt) to Math.max(y1.toInt, y2.toInt)))
 			case hRegex(x1, y, x2) => Some(HLine(Math.min(x1.toInt, x2.toInt) to Math.max(x1.toInt, x2.toInt), y.toInt))
 			case dRegex(x1, y1, x2, y2) => Some(DLine(x1.toInt, y1.toInt, x2.toInt, y2.toInt))
 			case _ => None
 		}).toIndexedSeq
+		val counts = parsedLines map {
+			case l: Line => l.count
+			case _ => 0
+		}
+		println(counts.sum)
 		genMap(parsedLines)
 	}
 
@@ -38,7 +43,7 @@ object Day5 extends Main {
 					map(x + 1000 * y) += 1
 				}
 				case d: DLine => {
-					for ((x, y) <- d.xRange zip d.yRange) {
+					for ((x, y) <- d.range) {
 						map(x + 1000 * y) += 1
 					}
 				}
@@ -47,7 +52,7 @@ object Day5 extends Main {
 		println(map.count(_ >= 2))
 	}
 
-	// Doesn't generalize nicely to
+	// Doesn't generalize nicely to diagonals
 	def genOverlaps(lines: IndexedSeq[Line]): Set[Position] = {
 		val pBuilder = Set.newBuilder[Position]
 		var overlaps = 0
@@ -89,12 +94,20 @@ object Day5 extends Main {
 
 	case class Position(x: Int, y: Int)
 
-	sealed trait Line
-	case class HLine(x: Range, y: Int) extends Line
-	case class VLine(x: Int, y: Range) extends Line
+	sealed trait Line {
+		def count: Int
+	}
+	case class HLine(x: Range, y: Int) extends Line {
+		val count: Int = x.length
+	}
+	case class VLine(x: Int, y: Range) extends Line {
+		val count: Int = y.length
+	}
 	case class DLine(x1: Int, y1: Int, x2: Int, y2: Int) extends Line {
-		val xRange: Range = if (x1 < x2) Range(x1, x2) else Range(x1, x2, -1)
-		val yRange: Range = if (y1 < y2) Range(y1, y2) else Range(y1, y2, -1)
+		val xRange: Range = if (x1 < x2) Range.inclusive(x1, x2) else Range.inclusive(x1, x2, -1)
+		val yRange: Range = if (y1 < y2) Range.inclusive(y1, y2) else Range.inclusive(y1, y2, -1)
 		assert(xRange.length == yRange.length)
+		val range: IndexedSeq[(Int, Int)] = xRange zip yRange
+		val count: Int = range.length
 	}
 }
