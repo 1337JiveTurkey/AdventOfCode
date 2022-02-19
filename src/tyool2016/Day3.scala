b8f2fa3b7c66eb97112c4f8d5abed5712e9c02eb
1337JiveTurkey <1337JiveTurkey@gmail.com>
2022-02-19 13:31:46 -0500

    2016 Day 3 Completed

--- src/tyool2016/Day3.scala
+++ src/tyool2016/Day3.scala
@@ -4,7 +4,7 @@ import scala.util.matching.Regex
 
 object Day3 extends Main {
 	def main(args: Array[String]): Unit = {
-		star1()
+		star2()
 	}
 
 	val numbers: Regex = """\s+(\d+)\s+(\d+)\s+(\d+)""".r
@@ -14,6 +14,18 @@ object Day3 extends Main {
 			case numbers(first, second, third) => List(first.toInt, second.toInt, third.toInt).sorted
 		}
 		println(triples.count(t => t(0) + t(1) > t(2)))
+	}
+
+	def star2(): Unit = {
+		val lines = fileLines("Day3.txt")
+		val triples = lines.map {
+			case numbers(first, second, third) => List(first.toInt, second.toInt, third.toInt)
+		}
+		val rotated = for {
+			groups <- triples.grouped(3)
+			triple <- groups.transpose
+		} yield triple.sorted
+		println(rotated.count(t => t(0) + t(1) > t(2)))
 	}
 
 }
