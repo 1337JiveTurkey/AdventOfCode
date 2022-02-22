386a95b7db6eeda27a6a4294237097520a60dcb9
1337JiveTurkey <1337JiveTurkey@gmail.com>
2022-02-22 00:17:02 -0500

    2016 Day 5 Complete

--- src/tyool2016/Day5.scala
+++ src/tyool2016/Day5.scala
@@ -1,8 +1,10 @@
 package tyool2016
 
+import scala.collection.mutable
+
 object Day5 extends Main {
 	def main(args: Array[String]): Unit = {
-		star1()
+		star2()
 	}
 
 	def star1(): Unit = {
@@ -11,6 +13,28 @@ object Day5 extends Main {
 		val filtered = hashes.filter(_.startsWith("00000"))
 		for (i <- filtered.take(8)) {
 			println(i)
+		}
+	}
+
+	def star2(): Unit = {
+		val prefix = "uqwqemis"
+		val hashes = for (i <- (0 to Int.MaxValue).iterator) yield md5(prefix + i)
+		val filtered = hashes.filter(_.startsWith("00000"))
+		val password = mutable.ArrayBuffer.from("********")
+		for (i <- filtered) {
+			val positionChar = i.drop(5).head
+			if (positionChar.isDigit) {
+				val position = positionChar.toString.toInt
+				if (position < 8 && password(position) == '*') {
+					val passwordChar = i.drop(6).head
+					password(position) = passwordChar
+					println(s"Password char $position is $passwordChar")
+				}
+			}
+			if (password.forall(_.isDigit)) {
+				println(password)
+				return
+			}
 		}
 	}
 }
