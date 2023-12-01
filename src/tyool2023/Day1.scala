0f5daabb2635f421a4613f7fd6d697697ac8b768
1337JiveTurkey <1337JiveTurkey@gmail.com>
2023-12-01 03:31:50 -0500

    2023 Day 1 Star 2

--- src/tyool2023/Day1.scala
+++ src/tyool2023/Day1.scala
@@ -5,9 +5,10 @@ import scala.util.matching.Regex
 object Day1 extends Main {
 	val firstDigit: Regex = """(?<=^\D*)\d""".r
 	val lastDigit: Regex = """\d(?=\D*$)""".r
+	val textDigits: Regex = """(one|two|three|four|five|six|seven|eight|nine)""".r
 
 	def main(args: Array[String]): Unit = {
-		star1()
+		star2()
 	}
 
 	def star1(): Unit = {
@@ -16,17 +17,41 @@ object Day1 extends Main {
 		println(lines.map(getCalibrationValue).sum)
 	}
 
-	def getCalibrationValue(str: String): Int = {
+	def star2(): Unit = {
+		val lines = fileLines("Day1.txt")
+
+		println(lines.map(textToDigits).map(getCalibrationValue).sum)
+	}
+
+	private def getCalibrationValue(str: String): Int = {
 		val number = firstDigitInString(str) + lastDigitInString(str)
 		number.toInt
 	}
 
-	def firstDigitInString(str: String): String = {
+	private def firstDigitInString(str: String): String = {
 		firstDigit.findFirstIn(str).get
 	}
 
-	def lastDigitInString(str: String): String = {
+	private def lastDigitInString(str: String): String = {
 		lastDigit.findFirstIn(str).get
+	}
+
+	private def textToDigits(str: String): String = {
+		replacement(replacement(str))
+	}
+
+	private def replacement(str: String): String = {
+		textDigits.replaceAllIn(str, digit => digit.group(0) match {
+			case "one" => "1e"
+			case "two" => "2o"
+			case "three" => "3e"
+			case "four" => "4r"
+			case "five" => "5e"
+			case "six" => "6x"
+			case "seven" => "7n"
+			case "eight" => "8t"
+			case "nine" => "9e"
+		})
 	}
 
 }
