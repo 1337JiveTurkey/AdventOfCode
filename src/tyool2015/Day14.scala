64c44e4006c78ef7f7495f1ef4ded1835e114d8b
1337JiveTurkey <1337JiveTurkey@gmail.com>
2023-12-04 07:39:46 -0500

    2015 Day 14 Star 1

---
+++ src/tyool2015/Day14.scala
@@ -0,0 +1,78 @@
+package tyool2015
+
+import scala.util.matching.Regex
+
+object Day14 extends Main {
+	val reindeerPattern: Regex = """(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.""".r
+
+	private val totalTime: Int = 2503
+
+	def main(args: Array[String]): Unit = {
+		star2()
+	}
+
+	def star1(): Unit = {
+		val lines = fileLines("Day14.txt")
+		val reindeer = lines map {
+			case reindeerPattern(name, speed, onTime, offTime) =>
+				Reindeer(name, speed.toInt, onTime.toInt, offTime.toInt)
+		}
+		for (r <- reindeer) {
+			val cycles = totalTime / r.cycleTime
+			val leftoverTime = totalTime % r.cycleTime
+			val lastOnTime = Math.min(r.onTime, leftoverTime)
+			val distance = (cycles * r.onTime + lastOnTime) * r.speed
+			println(r.name + ": " + distance)
+		}
+	}
+
+	def star2(): Unit = {
+		val lines = fileLines("Day14Prime.txt")
+		val reindeer = lines map {
+			case reindeerPattern(name, speed, onTime, offTime) =>
+				Reindeer(name, speed.toInt, onTime.toInt, offTime.toInt)
+		}
+		for (t <- 1 to 1000) {
+			var bestDistance = 0
+			for (r <- reindeer) {
+				val distance = r.simulate()
+				if (bestDistance < distance) {
+					bestDistance = distance
+				}
+			}
+			for (r <- reindeer) {
+				if (r.distance == bestDistance) {
+					r.points += 1
+				}
+			}
+		}
+		for (r <- reindeer) {
+			println(r.name + ": " + r.distance + " " + r.points)
+		}
+	}
+
+	case class Reindeer(name: String, speed: Int, onTime: Int, offTime: Int) {
+		val cycleTime: Int = onTime + offTime
+
+		var distance = 0
+		var points = 0
+
+		var onTimeRemaining: Int = onTime
+		var offTimeRemaining: Int = offTime
+
+		def simulate(): Int = {
+			if (onTimeRemaining > 0) {
+				onTimeRemaining -= 1
+				distance += speed
+			}
+			else if (offTimeRemaining > 0) {
+				offTimeRemaining -= 1
+			}
+			else {
+				onTimeRemaining = onTime
+				offTimeRemaining = offTime
+			}
+			distance
+		}
+	}
+}
