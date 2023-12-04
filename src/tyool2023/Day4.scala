e642ce744edf09dd3fdb2e45c145de9ba75890a7
1337JiveTurkey <1337JiveTurkey@gmail.com>
2023-12-04 02:07:07 -0500

    2023 Day 4 Star 2

--- src/tyool2023/Day4.scala
+++ src/tyool2023/Day4.scala
@@ -4,7 +4,7 @@ import scala.util.matching.Regex
 
 object Day4 extends Main {
 	def main(args: Array[String]): Unit = {
-		star1()
+		star2()
 	}
 
 	/**
@@ -19,22 +19,55 @@ object Day4 extends Main {
 
 		val cards = lines map {
 			case cardPattern(cardNumber, winningNumbers, gameNumbers) =>
-				Card(cardNumber.toInt, whitespaceDividedNumbers(winningNumbers).toSet, whitespaceDividedNumbers(gameNumbers).toSet)
+				Card(cardNumber.toInt - 1, whitespaceDividedNumbers(winningNumbers).toSet, whitespaceDividedNumbers(gameNumbers).toSet)
 		}
 		for (card <- cards) {
-			println(card.value)
+			println(card.numberOfMatches)
 		}
 		println(cards.map(_.value).sum)
 	}
 
+	def star2(): Unit = {
+		val lines = fileLines("Day4.txt")
+
+		val cards = lines map {
+			case cardPattern(cardNumber, winningNumbers, gameNumbers) =>
+				Card(cardNumber.toInt - 1, whitespaceDividedNumbers(winningNumbers).toSet, whitespaceDividedNumbers(gameNumbers).toSet)
+		}
+		val cardCounts = new Array[Int](cards.length)
+		var totalCopies = 0
+		for (card <- cards) {
+			val n = card.cardNumber
+			// Add the original card to the counts
+			cardCounts(n) += 1
+			val copies = cardCounts(n)
+			for (i <- 1 to card.numberOfMatches) {
+				cardCounts(n + i) += copies
+			}
+			totalCopies += copies
+		}
+		println(totalCopies)
+	}
+
+	/**
+	 * A scratch-off card with two sets of numbers. The card is part of a game
+	 * where the numbers on the right are winning if they match any of the numbers
+	 * on the left hand side.
+	 *
+	 * @param cardNumber The number of the card in the file.
+	 * @param winningNumbers The set of winning numbers on the card itself.
+	 * @param gameNumbers The set of selected numbers for the game.
+	 */
 	case class Card(cardNumber: Int, winningNumbers: Set[Int], gameNumbers: Set[Int]) {
 		lazy val matchingNumbers: Set[Int] = winningNumbers intersect gameNumbers
+		lazy val numberOfMatches: Int = matchingNumbers.size
+
 		lazy val value: Int = {
-			if (matchingNumbers.isEmpty) {
+			if (numberOfMatches == 0) {
 				0
 			}
 			else {
-				1 << (matchingNumbers.size - 1)
+				1 << (numberOfMatches - 1)
 			}
 		}
 	}
