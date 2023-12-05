1a3fb547374aba460cc0267b56a999496e5ffef1
1337JiveTurkey <1337JiveTurkey@gmail.com>
2023-12-05 04:00:43 -0500

    2015 Day 13 Star 2

--- src/tyool2015/Day13.scala
+++ src/tyool2015/Day13.scala
@@ -8,7 +8,7 @@ object Day13 extends Main {
 	val guestPattern: Regex = """(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+).""".r
 
 	def main(args: Array[String]): Unit = {
-		star1()
+		star2()
 	}
 
 	def star1(): Unit = {
@@ -16,6 +16,38 @@ object Day13 extends Main {
 
 		val mirth = new mutable.HashMap[(String, String), Int]
 		val names = new mutable.HashSet[String]
+		for (guestPattern(g1, gainLoss, amount, g2) <- lines) {
+			val pair = (g1, g2)
+			names.add(g1)
+			names.add(g2)
+			val happiness = if (gainLoss == "gain")
+				amount.toInt
+			else
+				-amount.toInt
+			mirth.put(pair, happiness)
+		}
+
+		var highestTotal = 0
+		for (arrangement <- names.toIndexedSeq.permutations) {
+			var total = 0
+			val extraPair = (arrangement.head, arrangement.last)
+			val reversedExtraPair = (arrangement.last, arrangement.head)
+			total += mirth.getOrElse(extraPair, 0) + mirth.getOrElse(reversedExtraPair, 0)
+			for ((first, second) <- pairwise(arrangement.iterator)) {
+				total += mirth.getOrElse((first, second), 0) + mirth.getOrElse((second, first), 0)
+			}
+			if (total > highestTotal) {
+				highestTotal = total
+			}
+		}
+		println(highestTotal)
+	}
+
+	def star2(): Unit = {
+		val lines = fileLines("Day13.txt")
+
+		val mirth = new mutable.HashMap[(String, String), Int]
+		val names = new mutable.HashSet[String].addOne("Me")
 		for (guestPattern(g1, gainLoss, amount, g2) <- lines) {
 			val pair = (g1, g2)
 			names.add(g1)
