ab96a3ae668efd485146996a21cb828832250c0c
1337JiveTurkey <1337JiveTurkey@gmail.com>
2023-12-06 01:22:48 -0500

    2023 Day 6 Star 2

--- src/tyool2023/Day6.scala
+++ src/tyool2023/Day6.scala
@@ -4,7 +4,7 @@ import scala.util.matching.Regex
 
 object Day6 extends Main {
 	def main(args: Array[String]): Unit = {
-		star1()
+		star2()
 	}
 
 	val numberPattern: Regex = """\d+""".r
@@ -22,10 +22,22 @@ object Day6 extends Main {
 		}
 	}
 
-	case class Race(time: Int, record: Int) {
-		def distance(x: Int): Int = (time - x) * x
-		def winner(x: Int): Boolean = distance(x) > record
+	def star2(): Unit = {
+		val lines = fileLines("Day6.txt")
+		val timesLine = lines(0)
+		val recordsLine = lines(1)
 
-		def numberOfWinners: Int = (1 until time).count(winner)
+		val time = numberPattern.findAllIn(timesLine).fold("")(_ + _)
+		val record = numberPattern.findAllIn(recordsLine).fold("")(_ + _)
+		val race = Race(time.toLong, record.toLong)
+
+		println(race.numberOfWinners)
+	}
+
+	case class Race(time: Long, record: Long) {
+		def distance(x: Long): Long = (time - x) * x
+		def winner(x: Long): Boolean = distance(x) > record
+
+		def numberOfWinners: Int = Range.Long(1, time, 1).count(winner)
 	}
 }
