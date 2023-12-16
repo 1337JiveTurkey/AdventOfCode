5bb241e8e82b087c7066e580c7583f7bdb553391
1337JiveTurkey <1337JiveTurkey@gmail.com>
2023-12-16 03:19:08 -0500

    2023 Day 15 Star 2

--- src/tyool2023/Day15.scala
+++ src/tyool2023/Day15.scala
@@ -1,14 +1,44 @@
 package tyool2023
 
+import scala.collection.mutable
+import scala.util.matching.Regex
+
 object Day15 extends Main {
 	def main(args: Array[String]): Unit = {
-		star1()
+		star2()
 	}
 
 	def star1(): Unit = {
 		val line = fileLine("Day15.txt")
-		val parts = line.split(',')
-		println(parts.map(hash).sum)
+		val instructions = line.split(',')
+		println(instructions.map(hash).sum)
+	}
+
+	def star2(): Unit = {
+		val line = fileLine("Day15.txt")
+		val instructions = line.split(',') map {
+			case instructionPattern(label, op, focal) => Instruction(label, hash(label), op(0), focal.toIntOption)
+		}
+		val boxes = new Array[mutable.LinkedHashMap[String, Int]](256)
+		for (i <- boxes.indices) {
+			boxes(i) = new mutable.LinkedHashMap[String,Int]
+		}
+		for (i <- instructions) {
+			val boxN = i.box
+			if (i.op == '=') {
+				boxes(boxN).put(i.label, i.focal.get)
+			} else {
+				boxes(boxN).remove(i.label)
+			}
+		}
+		var sum = 0
+		for (i <- boxes.indices) {
+			val box = boxes(i)
+			for ((focus, slot) <- box.values.zipWithIndex) {
+				sum += (i + 1) * focus * (slot + 1)
+			}
+		}
+		println(sum)
 	}
 
 	def hash(in: String): Int = {
@@ -20,4 +50,7 @@ object Day15 extends Main {
 		}
 		acc
 	}
+
+	val instructionPattern: Regex = """([a-z]+)([=\-])(\d?)""".r
+	case class Instruction(label: String, box: Int, op: Char, focal: Option[Int])
 }
